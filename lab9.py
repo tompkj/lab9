f2506e687a05778a6d93956cb43a2cf09800c197
jdlowman2 <jdlowman2@gmail.com>
2016-11-25 23:08:30 -0500

    Passed 43 of 52 offline tests

--- lab9.py
+++ lab9.py
@@ -71,7 +71,22 @@ def get_overall_misclassifications(H, training_points, classifier_to_misclassifi
     dictionary mapping classifiers to the training points they misclassify,
     returns a set containing the training points that H misclassifies.
     H is represented as a list of (classifier, voting_power) tuples."""
-    raise NotImplementedError
+    misclassified = []
+    
+    
+    for p in training_points:
+        vote = 0
+        for tup in H:
+            classifier = tup[0]
+            if p in classifier_to_misclassified[classifier]:
+                vote-= tup[1]
+            else:
+                vote+=tup[1]
+            
+        if vote <= 0:
+            misclassified.append(p)
+    
+    return set(misclassified)
 
 def is_good_enough(H, training_points, classifier_to_misclassified, mistake_tolerance=0):
     """Given an overall classifier H, a list of all training points, a
@@ -80,7 +95,8 @@ def is_good_enough(H, training_points, classifier_to_misclassified, mistake_tole
     returns False if H misclassifies more points than the tolerance allows,
     otherwise True.  H is represented as a list of (classifier, voting_power)
     tuples."""
-    raise NotImplementedError
+    set_misclassified = get_overall_misclassifications(H, training_points, classifier_to_misclassified)
+    return len(set_misclassified) <= mistake_tolerance
 
 def update_weights(point_to_weight, misclassified_points, error_rate):
     """Given a dictionary mapping training points to their old weights, a list
@@ -88,16 +104,49 @@ def update_weights(point_to_weight, misclassified_points, error_rate):
     error rate of the current weak classifier, returns a dictionary mapping
     training points to their new weights.  This function is allowed (but not
     required) to modify the input dictionary point_to_weight."""
-    raise NotImplementedError
+    new_weight_dict = {}
+    
+    for p in point_to_weight.keys():
+        old_weight = point_to_weight[p]
+        if p not in misclassified_points:
+            new_weight = make_fraction(1,2)*1/(1-error_rate)*old_weight
+        else:
+            new_weight = make_fraction(1,2)*1/(error_rate)*old_weight
+        new_weight_dict[p]= new_weight
+        
+    return new_weight_dict
 
 def adaboost(training_points, classifier_to_misclassified,
              use_smallest_error=True, mistake_tolerance=0, max_rounds=INF):
     """Performs the Adaboost algorithm for up to max_rounds rounds.
     Returns the resulting overall classifier H, represented as a list of
     (classifier, voting_power) tuples."""
-    raise NotImplementedError
-
-
+    
+    point_to_weight = initialize_weights(training_points)
+    exit_condition1 = False
+    exit_condition2 = False
+    exit_condition3 = False
+    rounds = 0
+    H = []
+    
+    while not exit_condition1 and not exit_condition2 and not exit_condition3:
+        rounds +=1
+        classifier_to_error_rate = calculate_error_rates(point_to_weight, classifier_to_misclassified)
+        try:
+            h = pick_best_classifier(classifier_to_error_rate, use_smallest_error)
+        except NoGoodClassifiersError:
+            exit_condition3 = True
+            
+        voting_power = calculate_voting_power(classifier_to_error_rate[h])
+        H.append((h, voting_power))
+        H_misclassified_points = get_overall_misclassifications(H, training_points, classifier_to_misclassified)
+        point_to_weight = update_weights(point_to_weight, H_misclassified_points, classifier_to_error_rate[h])      
+        
+        exit_condition1 = is_good_enough(H, training_points, classifier_to_misclassified, mistake_tolerance)
+        exit_condition2 = (rounds >= max_rounds)
+        
+    return H
+    
 #### SURVEY ####################################################################
 
 NAME = "Joseph Lowman"
